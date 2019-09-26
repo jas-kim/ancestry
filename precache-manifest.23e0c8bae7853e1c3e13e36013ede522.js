b6ce73863cc2206e9e4d5026631b203b41a4a23b
Jason K <jasonyangkim@gmail.com>
2019-09-25 17:12:47 -0700

    Updates

---
+++ precache-manifest.23e0c8bae7853e1c3e13e36013ede522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c63e98c61a781218fdc2834c2393071",
+    "url": "/getbozak/index.html"
+  },
+  {
+    "revision": "09644e1b7ab8bc1cfc6b",
+    "url": "/getbozak/static/css/main.1533ca87.chunk.css"
+  },
+  {
+    "revision": "4b3f610a4143cf87fbc4",
+    "url": "/getbozak/static/js/2.e5438a99.chunk.js"
+  },
+  {
+    "revision": "09644e1b7ab8bc1cfc6b",
+    "url": "/getbozak/static/js/main.b93aba7a.chunk.js"
+  },
+  {
+    "revision": "80a457270e74a3b986fe",
+    "url": "/getbozak/static/js/runtime~main.3ca43ead.js"
+  }
+]);
